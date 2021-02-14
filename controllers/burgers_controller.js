093d43fbaebe5c589ab26164302d3f33aea55d36
Josh Rehanek <joshrehanek@gmail.com>
2021-02-13 16:55:27 -0800

    initial commit & setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const burger = require('../models/burger');
+
+//create router for app 
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+const orm = require('../config/orm')
+
+//create code that will call ORM function using burger specific input for the ORM
+
+module.exports = burger;
