d9174570b58435ff4347ba4f2dac367cbfebf6a3
Josh Rehanek <joshrehanek@gmail.com>
2021-02-13 16:53:56 -0800

    adds handlebars and app.listen

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 // Initialize the app and create a port
 const app = express();
@@ -6,3 +7,10 @@ const PORT = process.env.PORT || 3000;
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
