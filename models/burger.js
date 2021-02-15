44d10286faaec6fe139671470e22e6a9dfb3b487
Josh Rehanek <joshrehanek@gmail.com>
2021-02-14 19:54:16 -0800

    adds all orm burger methods

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,23 @@
-const orm = require('../config/orm')
+const orm = require('../config/orm');
 
 //create code that will call ORM function using burger specific input for the ORM
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 module.exports = burger;
