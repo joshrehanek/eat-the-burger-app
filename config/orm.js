c791ea0ede29e79684a7d674f202b235a49e8078
Josh Rehanek <joshrehanek@gmail.com>
2021-02-14 19:53:28 -0800

    adds selectAll & insertOne funcs

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,48 @@
 const connection = require("./connection.js");
 
+// / Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+  // column1=value, column2=value2,...
+  const arr = [];
+  for (const key in ob) {
+    arr.push(key + '=' + ob[key]);
+  }
+  return arr.toString();
+}
+const printQuestionMarks = (num) => {
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
+  return arr.toString();
+}
+
 const orm = {
-    selectAll: function(table, colToSearch) {
-        const query = "SELECT * FROM ??";
-        connection.query(query, [table, colToSearch], function (err, result) {
+    selectAll: function(table, cb) {
+        const query = `SELECT * FROM ${table};`;
+        connection.query(query, function (err, result) {
             if (err) throw err;
+            else cb(result);
             console.log(result);
         });
-    }
+    },
+    insertOne: function (table, cols, vals, cb) {
+      let queryString = 'INSERT INTO ' + table;
+      queryString += ' (';
+      queryString += cols.toString();
+      queryString += ') ';
+      queryString += 'VALUES (';
+      queryString += printQuestionMarks(vals.length);
+      queryString += ') ';
+      // console.log(`This is the query string from insert: ${queryString}`);
+      connection.query(queryString, vals, function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+  
 };
 
 insertOne()
